03bffaf8916d21a8292c28ae1bad4e9ac4355d09
MayankGoyal1805 <178543798+MayankGoyal1805@users.noreply.github.com>
2025-05-10 13:19:36 +0530

    huha

--- numpy/durer.py
+++ numpy/durer.py
@@ -7,13 +7,28 @@ square = np.array([
     [4, 15, 14, 1]
 ])
 
-def test_row_col():
+def test_row():
     for i in range(4):
-        assert square[:, i].sum() == 34
-        assert square[i, :].sum() == 34
+        assert(square[i, :].sum()) == 34
 
+def test_col():
+    for i in range(4):
+        assert(square[:, i].sum()) == 34
 
+def test_top_left():
+    assert(square[:2,:2].sum()) == 34
 
+def test_bottom_left():
+    assert(square[2:,:2].sum()) == 34
 
+def test_top_right():
+    assert(square[:2,2:].sum()) == 34
 
+def test_bottom_right():
+    assert(square[2:,2:].sum()) == 34   
 
+def test_corners():
+    assert(square[0,0]+square[3,0]+square[3,3]+square[0,3]) == 34
+
+def test_center_four():
+    assert(square[1,1]+square[1,2]+square[2,2]+square[2,1]) == 34  
